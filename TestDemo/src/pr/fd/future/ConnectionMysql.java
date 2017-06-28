d7cd74e0f2bd07049a9b559a8fa7fd096625cb27
future <1627739846@qq.com>
2017-06-28 10:34:27 +0800

    update the connectionMysql.java

--- TestDemo/src/pr/fd/future/ConnectionMysql.java
+++ TestDemo/src/pr/fd/future/ConnectionMysql.java
@@ -2,31 +2,141 @@ package pr.fd.future;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * 数据库连接与关闭类
+ * 
+ * @author
+ * 
+ */
 public class ConnectionMysql {
 
-	String driver = "com.mysql.jdbc.Driver";
-	String url = "jdbc:mysql://localhost/alk";
-
-	String user = "root";
-	String pwd = "root";
-
-	public Connection getconnection() {
-		Connection conn = null;
+	public Connection conn;
+	public PreparedStatement pstmt;
+	public ResultSet rs;
+	static {
 		try {
-			Class.forName(driver);
-			conn = DriverManager.getConnection(url, user, pwd);
-		} catch (ClassNotFoundException | SQLException e) {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 与数据库获取连接
+	 * 
+	 * @return
+	 */
+	public Connection getConnection() {
+		try {
+			conn = DriverManager.getConnection(
+					"jdbc:sqlserver://localhost:1433;DatabaseName=epet", "sa",
+					"");
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return conn;
 	}
 
+	/**
+	 * 关闭数据库相关的资源
+	 * 
+	 * @param conn
+	 *            数据库连接对象
+	 * @param pstmt
+	 *            语句对象
+	 * @param rs
+	 *            结果集
+	 */
+	public void closeAll(Connection con, PreparedStatement pstmt, ResultSet rs) {
+		if (null != rs) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+			}
+		}
+
+		if (null != pstmt) {
+			try {
+				pstmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		if (null != con) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * 执行增、删、改操作
+	 * 
+	 * @param sql
+	 *            预处理的SQL语句
+	 * @param param
+	 *            语句对象
+	 * @return 受影响行数
+	 */
+	public int executeUpdate(String sql, Object param[]) {
+		conn = getConnection();
+		int num = 0;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			if (null != param && param.length > 0) {
+				for (int i = 0; i < param.length; i++) {
+					pstmt.setObject(i + 1, param[i]);
+				}
+			}
+			num = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			closeAll(conn, pstmt, rs);
+		}
+		return num;
+	}
+
+	/**
+	 * 执行查操作
+	 * 
+	 * @param sql
+	 *            预处理的SQL语句
+	 * @param param
+	 *            语句对象
+	 * @return 受影响行数
+	 */
+	public ResultSet executeQuery(String sql, Object param[]) {
+		conn = getConnection();
+		try {
+			pstmt = conn.prepareStatement(sql);
+			if (null != param && param.length > 0) {
+				for (int i = 0; i < param.length; i++) {
+					pstmt.setObject(i + 1, param[i]);
+				}
+			}
+			rs = pstmt.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rs;
+	}
+
 	public static void main(String[] args) {
-		ConnectionMysql test = new ConnectionMysql();
-		System.out.println(test.getconnection());
+		System.out.println(new ConnectionMysql().getConnection());
 	}
 
 }
